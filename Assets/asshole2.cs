317c8136e0717584fd2704e3e252cb26194408cd
Mrpiyan <marxlin2006@gmail.com>
2023-09-09 17:02:30 +0800

    初次上傳
    初次上船拉幹

---
+++ Assets/asshole2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asshole2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    Debug.Log("asdf");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
